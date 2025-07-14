e073399f6a66d6e9271087a16ea4253d2575bae3
Amirthavahini <amirtha.vahini14@gmail.com>
2025-07-14 13:08:15 +0530

    first week problem

---
+++ week1.rb
@@ -0,0 +1,23 @@
+print "Enter your name: "
+name = gets.chomp
+print "Enter your phone number: "
+number = gets.chomp
+print "Enter your email id: "
+email = gets.chomp
+print "Enter your branch: "
+branch = gets.chomp
+print "Which year are you in: "
+year = gets.chomp
+print "Which place are you coming from: "
+place = gets.chomp
+print "Famous place to visit in your native place: "
+famous = gets.chomp
+
+
+puts "Hi I am Mr./Mrs #{name}.
+My Phone Number is +91#{number}.
+My email id is #{email}.
+I am studying #{branch}.
+I am currently in #{year} year.
+I am coming from #{place}.
+#{famous} is the famous place to visit in my native place."
