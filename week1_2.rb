960f940b50b5180fe705811a2ca97fe540fc7db9
Amirthavahini <amirtha.vahini14@gmail.com>
2025-07-21 12:42:10 +0530

    Create week1_2.rb

---
+++ week1_2.rb
@@ -0,0 +1,23 @@
+#Area of circle
+print "Enter radius of circle: "
+radius = gets.chomp.to_i
+puts "Area of circle is #{3.14*radius*radius} units"
+
+#Area of square
+print "Enter length of square: "
+side = gets.chomp.to_i
+puts "Area of square is #{side*side} units"
+
+#Area of rectangle
+print "Enter length of rectangle: "
+length = gets.chomp.to_i
+print "Enter breadth of rectangle: "
+breadth = gets.chomp.to_i
+puts "Area of rectangle is #{length*breadth} units"
+
+#Area of parallelogram
+print "Enter length of parallelogram: "
+length_p = gets.chomp.to_i
+print "Enter height of parallelogram: "
+height_p = gets.chomp.to_i
+puts "Area of parallelogram is #{length_p*height_p} units"
